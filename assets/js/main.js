319eb4aaf2b070195af41f4994c6242d688e965a
nicolaazonta <nicolazonta114@gmail.com>
2023-03-21 15:36:58 +0100

    generate five random numbers into the dom

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+/* Visualizzare in pagina 5 numeri casuali. Da lì parte un timer di 30 secondi.
+Dopo 30 secondi i numeri scompaiono e l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati. */
+
+let randomNumbersPc = [];
+for (let i = 0; i < 5; i++) {
+
+    randomNumbersPc.push(Math.floor(Math.random() * 6))
+
+    const numberEl = document.querySelector('.container')
+
+    const numberSquare = `<div class="number_square"><span>${randomNumbersPc[i]}</span></div>`
+
+    numberEl.insertAdjacentHTML('beforeend', numberSquare)
+
+}
+console.log(randomNumbersPc);
